480f0ed7c326213fe600f4cef72f957886b119a8
Dalton Craven <daltoncraven@proton.me>
2023-05-16 00:11:09 -0400

    chore: move eslintrc into separate file

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ['@redwoodjs/eslint-config'],
+  root: true,
+  rules: {
+    '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+    '@typescript-eslint/consistent-type-imports': ['error'],
+  },
+}
