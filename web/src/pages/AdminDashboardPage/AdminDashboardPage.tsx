52b242a64d613c87138292632d96a4ab7a084ad3
claycrews2002 <claycrews2002@gmail.com>
2023-05-30 10:39:21 -0400

    feat: admin dashboard

--- web/src/pages/AdminDashboardPage/AdminDashboardPage.tsx
+++ web/src/pages/AdminDashboardPage/AdminDashboardPage.tsx
@@ -1,20 +1,38 @@
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 
+const DASHBOARD_ITEMS = [
+  {
+    title: 'Add User',
+    content: 'Fill out a `create new user` form.',
+    to: routes.createUser(),
+  },
+  {
+    title: 'Modify User',
+    content: 'Delete or Update information for an existing user.',
+    to: routes.modifyUser(),
+  },
+] as const
+
 const AdminDashboardPage = () => {
   return (
     <>
       <MetaTags title="AdminDashboard" description="AdminDashboard page" />
 
-      <h1>AdminDashboardPage</h1>
-      <p>
-        Find me in{' '}
-        <code>./web/src/pages/AdminDashboardPage/AdminDashboardPage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>adminDashboard</code>, link to me with `
-        <Link to={routes.adminDashboard()}>AdminDashboard</Link>`
-      </p>
+      <ul className="grid gap-4 sm:grid-cols-3">
+        {DASHBOARD_ITEMS.map((item) => (
+          <Link
+            to={item.to}
+            key={item.to}
+            className="card shadow-lg hover:shadow-xl"
+          >
+            <li className="card-body">
+              <h3 className="card-title">{item.title}</h3>
+              <p>{item.content}</p>
+            </li>
+          </Link>
+        ))}
+      </ul>
     </>
   )
 }
