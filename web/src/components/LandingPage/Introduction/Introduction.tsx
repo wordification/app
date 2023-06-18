54f3b6aa43ba31b0577fe519ba0ae0496ce4d378
claycrews2002 <claycrews2002@gmail.com>
2023-06-18 17:28:51 -0400

    feat: remove question

--- web/src/components/LandingPage/Introduction/Introduction.tsx
+++ web/src/components/LandingPage/Introduction/Introduction.tsx
@@ -7,13 +7,7 @@ const Introduction = () => {
             The Wordification® Project
           </h1>
           <h2 className="text-2xl font-semibold">By ScholasTech, LLC</h2>
-          <p className="py-6 text-xl">
-            Can dialectally responsive, linguistically based spelling
-            instruction provided through computer-based gamified instruction
-            support students’ spelling development across a variety of dialects,
-            to make literacy widely and universally available?
-          </p>
-          <ul className="list-disc py-2 pl-6 text-left text-xl">
+          <ul className="list-disc py-6 pl-6 text-left text-xl">
             <li>
               Current classroom spelling instruction is largely
               memorization-based and one-size-fits-all. Sophisticated,
