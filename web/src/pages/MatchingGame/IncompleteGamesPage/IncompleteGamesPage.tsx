1fcae7913ee58df96c1f68db8496a22afb7426ff
claycrews2002 <claycrews2002@gmail.com>
2023-06-21 13:42:34 -0400

    feat: front end matching game base

--- web/src/pages/MatchingGame/IncompleteGamesPage/IncompleteGamesPage.tsx
+++ web/src/pages/MatchingGame/IncompleteGamesPage/IncompleteGamesPage.tsx
@@ -1,22 +1,5 @@
-import { Link, routes } from '@redwoodjs/router'
-import { MetaTags } from '@redwoodjs/web'
-
 const MatchingGameIncompleteGamesPage = () => {
-  return (
-    <>
-      <MetaTags title="IncompleteGames" description="IncompleteGames page" />
-
-      <h1>IncompleteGamesPage</h1>
-      <p>
-        Find me in{' '}
-        <code>./web/src/pages/IncompleteGamesPage/IncompleteGamesPage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>incompleteGames</code>, link to me with
-        `<Link to={routes.incompleteGames()}>IncompleteGames</Link>`
-      </p>
-    </>
-  )
+  return <MatchingGamesCell complete={false} />
 }
 
 export default MatchingGameIncompleteGamesPage
