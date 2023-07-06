16b53f18b49a9e4056c35f0845899efa4b82dde7
claycrews2002 <claycrews2002@gmail.com>
2023-07-06 12:00:39 -0400

    feat: set up for sending emails

---
+++ api/db/migrations/20230706155542_email/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "Audit" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" INTEGER NOT NULL,
+    "log" TEXT NOT NULL,
+
+    CONSTRAINT "Audit_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Audit" ADD CONSTRAINT "Audit_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
