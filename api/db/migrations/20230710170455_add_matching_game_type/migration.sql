ca64c65c5a6f1f7071166f43d2ffc3e45df340aa
claycrews2002 <claycrews2002@gmail.com>
2023-07-12 15:39:25 -0400

    fix: null matchingGameType error

--- api/db/migrations/20230710170455_add_matching_game_type/migration.sql
+++ api/db/migrations/20230710170455_add_matching_game_type/migration.sql
@@ -1,14 +1,19 @@
-/*
-  Warnings:
-
-  - Added the required column `matchingGameType` to the `GameSetup` table without a default value. This is not possible if the table is not empty.
-
-*/
 -- CreateEnum
 CREATE TYPE "MatchingGameType" AS ENUM ('MEMORY', 'GROUPING');
 
 -- AlterTable
 ALTER TABLE "Game" ADD COLUMN     "matchingGameType" "MatchingGameType";
 
+-- UpdateTable
+UPDATE "Game"  SET "matchingGameType" = 'MEMORY' WHERE "matchingGameType" IS NULL;
+
 -- AlterTable
-ALTER TABLE "GameSetup" ADD COLUMN     "matchingGameType" "MatchingGameType" NOT NULL;
+ALTER TABLE "GameSetup" ADD COLUMN     "matchingGameType" "MatchingGameType";
+
+-- UpdateTable
+UPDATE "GameSetup"  SET "matchingGameType" = 'MEMORY' WHERE "matchingGameType" IS NULL;
+
+-- AlterTable
+ALTER TABLE "GameSetup"
+ALTER COLUMN "matchingGameType" SET NOT NULL,
+ALTER COLUMN "matchingGameType" SET DEFAULT 'MEMORY';
