f65d7e486362742a381639168b9a40beb10adf73
claycrews2002 <claycrews2002@gmail.com>
2023-07-12 14:23:30 -0400

    feat: get grapheme counts

--- scripts/getLongVowels.ts
+++ scripts/getLongVowels.ts
@@ -20,6 +20,14 @@ const fileData = fs.readFileSync(filePath, 'utf8')
 const words: Word[] = JSON.parse(fileData)
 
 // Filter words based on phonemes with one syllable
+const wordsWithLongI = words.filter(
+  (word) => word.phonemes.includes(49) && word.numSyllables === 1
+)
+const wordsWithLongO = words.filter(
+  (word) =>
+    (word.phonemes.includes(53) || word.phonemes.includes(46)) &&
+    word.numSyllables === 1
+)
 const wordsWithLongE = words.filter(
   (word) => word.phonemes.includes(45) && word.numSyllables === 1
 )
@@ -31,6 +39,69 @@ const wordsWithLongA = words.filter(
 const wordsWithLongU = words.filter(
   (word) => word.phonemes.includes(47) && word.numSyllables === 1
 )
+
+// Display number of each ow, oCe, oa, igh, iCe, y words
+const oneSyllWords = words.filter((word) => word.numSyllables === 1)
+
+const owWords = oneSyllWords.filter((word) => word.graphemes.includes('ow'))
+const oCeWords = oneSyllWords.filter((word) => word.graphemes.includes('oCe'))
+const oaWords = oneSyllWords.filter((word) => word.graphemes.includes('oa'))
+const ighWords = oneSyllWords.filter((word) => word.graphemes.includes('igh'))
+const iCeWords = oneSyllWords.filter((word) => word.graphemes.includes('iCe'))
+const yWords = oneSyllWords.filter((word) => word.graphemes.includes('y'))
+
+// Print the filtered words
+console.log('ow Words')
+owWords.forEach((word) => {
+  console.log(word.word)
+})
+console.log('# of ow Words: ', owWords.length)
+
+console.log('oCe Words')
+oCeWords.forEach((word) => {
+  console.log(word.word)
+})
+console.log('# of oCe Words: ', oCeWords.length)
+
+console.log('oa Words')
+oaWords.forEach((word) => {
+  console.log(word.word)
+})
+console.log('# of oa Words: ', oaWords.length)
+
+console.log('igh Words')
+ighWords.forEach((word) => {
+  console.log(word.word)
+})
+console.log('# of igh Words: ', ighWords.length)
+
+console.log('iCe Words')
+iCeWords.forEach((word) => {
+  console.log(word.word)
+})
+console.log('# of iCe Words: ', iCeWords.length)
+
+console.log('y Words')
+yWords.forEach((word) => {
+  console.log(word.word)
+})
+console.log('# of y Words: ', yWords.length)
+
+// Print the filtered words
+console.log('Long I')
+wordsWithLongI.forEach((word) => {
+  console.log(word.word)
+})
+// Display the count
+console.log(`Count: ${wordsWithLongI.length}`)
+
+// Print the filtered words
+console.log('Long O')
+wordsWithLongO.forEach((word) => {
+  console.log(word.word)
+})
+// Display the count
+console.log(`Count: ${wordsWithLongI.length}`)
 
 // Print the filtered words
 console.log('Long E')
