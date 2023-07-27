861671bed330e4c303282fe2998d4e5201f306d8
claycrews2002 <claycrews2002@gmail.com>
2023-07-27 11:46:32 -0400

    feat: add presentation video

--- web/src/pages/Landing/PresentationPage/PresentationPage.tsx
+++ web/src/pages/Landing/PresentationPage/PresentationPage.tsx
@@ -1,4 +1,3 @@
-import { Link } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 
 const LandingPresentationPage = () => {
@@ -10,34 +9,51 @@ const LandingPresentationPage = () => {
         <h1 className="text-xl font-semibold">CSCE 2023 Conference</h1>
       </div>
       <div className="hero mb-10 mt-10 rounded-lg bg-base-200">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          {/** Insert doc link here */}
-          <Link to="">
+        <div className="hero-content flex-col-reverse lg:flex-row-reverse">
+          {/** --vvv-- INSERT DOC LINK HERE WHE PUBLISHED --vvv-- */}
+          {/* <Link to="">
             <div className="card image-full w-72 flex-shrink-0 bg-base-100 shadow-xl transition hover:shadow-2xl">
               <figure>
-                {/** Insert doc image link here */}
+
                 <img
                   src="https://placehold.co/560x560"
                   alt="Wordification Paper"
                 />
               </figure>
             </div>
-          </Link>
+          </Link> */}
+          {/** --^^^-- INSERT DOC LINK HERE WHE PUBLISHED --^^^-- */}
+
+          {/** --vvv-- REMOVE WHEN INSERTING DOC --vvv-- */}
+          <div className="card flex h-72 w-72 items-center justify-center bg-base-100 shadow-xl transition hover:shadow-2xl">
+            <p className="text-center text-xl font-semibold">
+              Link to Publication Coming Soon!
+            </p>
+          </div>
+          {/** --^^^-- REMOVE WHEN INSERTING DOC --^^^-- */}
+
           <div className="text-center lg:text-left">
             <h1 className="text-5xl font-semibold">Wordification:</h1>
             <h1 className="text-5xl font-bold">
               A New Way of Teaching English Spelling Patterns [CSCE &apos;23]
             </h1>
-            <div className="py-6 italic">
-              <p className="py-2">
+            <div className="py-6">
+              <p className="py-2 italic">
                 2023 World Congress in Computer Science, Computer Engineering, &
                 Applied Computing (CSCE&apos;23)
               </p>
-              <p className="py-1">Las Vegas, 26 July 2023</p>
-              <p>
-                Lexington Whalen, Dalton Craven, Shashank Comandur, Nathan
-                Bickel, Homayoun Valafar, Stanley Dubinsky
+              <p className="py-1 italic">Las Vegas, 26 July 2023</p>
+              <p className="py-2 font-bold">
+                Lexington Whalen<sup>1</sup>, Dalton Craven<sup>1</sup>,
+                Shashank Comandur<sup>1</sup>, Nathan Bickel<sup>1</sup>,
+                Jackson Ginn<sup>1</sup>, Clay Crews<sup>1</sup>, Homayoun
+                Valafar<sup>1</sup>, & Stanley Dubinsky<sup>2</sup>
               </p>
+              <p className="py-1">
+                <sup>1</sup>Department of Computer Science & Engineering,
+                <sup>2</sup>Linguistics Program
+              </p>
+              <p className="font-semibold">University of South Carolina</p>
             </div>
           </div>
         </div>
@@ -55,7 +71,7 @@ const LandingPresentationPage = () => {
                 className="md:w-1120 h-630 w-full"
                 width="1120"
                 height="630"
-                src=""
+                src="https://www.youtube.com/embed/w6DAqp-X7Y0"
                 title="YouTube video player"
                 frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
