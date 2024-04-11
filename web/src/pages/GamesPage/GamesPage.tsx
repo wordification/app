2b47e203486bc3694e7ecda17a6b8db4e91d09d1
Shash Comandur <shashank.comandur@gmail.com>
2024-04-10 22:54:08 -0400

    style: added icons to games page

--- web/src/pages/GamesPage/GamesPage.tsx
+++ web/src/pages/GamesPage/GamesPage.tsx
@@ -1,16 +1,20 @@
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
+import SortingGameIcon from '../../../../static/SortingGameIcon.svg'
+import MatchingGameIcon from '../../../../static/MatchingGameIcon.svg'
 
 const GAME_CHOICES = [
   {
     title: 'Sorting',
     content: 'Sort the cards to win!',
     to: routes.sortingGame(),
+    icon: SortingGameIcon,
   },
   {
     title: 'Matching',
     content: 'Match the cards to win!',
     to: routes.matchingGame(),
+    icon: MatchingGameIcon,
   },
 ] as const
 
@@ -27,9 +31,13 @@ const GamesPage = () => {
             key={game.to}
             className="card shadow-lg hover:shadow-xl  bg-off-black"
           >
-            <li className="card-body">
-              <h3 className="card-title text-white text-5xl">{game.title}</h3>
-              <p className="text-gray text-2xl">{game.content}</p>
+            <li className="card-body flex flex-row justify-between">
+              <div className="">
+                <h3 className="card-title text-white text-5xl">{game.title}</h3>
+                <p className="text-gray text-2xl">{game.content}</p>
+              </div>
+
+              <object data={game.icon} type="image/svg+xml" className="w-1/4 pr-5"> </object>
             </li>
           </Link>
         ))}
